1f43bcd7ff50888155dedda7e67dc6d5a5e6382b
juandurefaccini <juanchidure@outlook.com>
2020-05-23 19:40:47 -0300

    Terminado EJ 4

--- src/edu/unicen/tp4/ej6_Abstraccion/SistemaAsignacion.java
+++ src/edu/unicen/tp4/ej6_Abstraccion/SistemaAsignacion.java
@@ -1,16 +1,25 @@
 package edu.unicen.tp4.ej6_Abstraccion;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class SistemaAsignacion {
     String nombre;
-    Collection<Evaluador> evaluadores = new ArrayList<>();
-    Collection<Trabajo> trabajos = new ArrayList<>();
-    Collection<String> temasEspecificos = new ArrayList<>(); //Uso coleccion porque no necesito acceder a un indice
+    Set<Evaluador> evaluadores = new HashSet<>();
+    Set<Trabajo> trabajos = new HashSet<>();
+    Set<String> temasEspecificos = new HashSet<>(); //Uso coleccion porque no necesito acceder a un indice
     //especifico ademas de permitirme hacer mas rapida la interseccion
+
+    public void addEvaluador(Evaluador evaluador){
+        evaluadores.add(evaluador);
+    }
+
+    public void addTrabajo(Trabajo trabajo){
+        trabajos.add(trabajo);
+    }
+
+    public void addTemasEspecificos(String tema){
+        temasEspecificos.add(tema);
+    }
 
     public SistemaAsignacion(String nombre) {
         this.nombre = nombre;
@@ -36,5 +45,58 @@ public class SistemaAsignacion {
             }
         }
         return Collections.unmodifiableCollection(posiblesTextos);
+    }
+
+    public static void main(String[] args) {
+        SistemaAsignacion sistemaAsignacion = new SistemaAsignacion("Conicet");
+
+        Evaluador ev = new Evaluador("Juan");
+        Evaluador ev2 = new Evaluador("Franco");
+        Evaluador ev3 = new Evaluador("Licha");
+
+        Trabajo t = new Trabajo("EP1");
+        Trabajo t2 = new Trabajo("EP2");
+        Trabajo t3 = new Trabajo("EP3");
+        Trabajo t4 = new Trabajo("EP4");
+
+        sistemaAsignacion.addTemasEspecificos("A");
+
+        t.addTema("A");
+        t.addTema("B");
+        t.addTema("C");
+
+        t2.addTema("A");
+        t2.addTema("B");
+        t2.addTema("C");
+
+        t3.addTema("A");
+        t3.addTema("B");
+//        t3.addTema("C");
+
+        t4.addTema("A");
+        t4.addTema("B");
+        t4.addTema("C");
+        t4.addTema("D");
+
+        ev.addTema("A");
+        ev.addTema("B");
+        ev.addTema("C");
+
+        ev2.addTema("A");
+        ev2.addTema("B");
+        ev2.addTema("C");
+
+        sistemaAsignacion.addEvaluador(ev);
+        sistemaAsignacion.addEvaluador(ev2);
+
+        sistemaAsignacion.addTrabajo(t);
+        sistemaAsignacion.addTrabajo(t2);
+        sistemaAsignacion.addTrabajo(t3);
+
+        System.out.println(sistemaAsignacion.posiblesEvaluadores(t));
+        System.out.println(sistemaAsignacion.posiblesTextos(ev));
+
+        System.out.println(sistemaAsignacion.esExperto(ev));
+
     }
 }
